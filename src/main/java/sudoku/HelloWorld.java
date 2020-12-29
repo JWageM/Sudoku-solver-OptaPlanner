690a94bdfee5bd2c60191db42b4793a4ee8a671c
Jens Wagemaker <jens.wagemaker@gmail.com>
2020-12-29 15:48:43 +0100

    update licence and remove comment

--- src/main/java/sudoku/HelloWorld.java
+++ src/main/java/sudoku/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 
 
 /*
- * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Jens Wagemaker.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,17 +28,6 @@ public class HelloWorld {
  * limitations under the License.
  */
 
-/*
-package org.optaplanner.examples.nqueens.app;
-
-import java.util.List;
-
-import org.optaplanner.core.api.solver.Solver;
-import org.optaplanner.core.api.solver.SolverFactory;
-import org.optaplanner.examples.nqueens.domain.NQueens;
-import org.optaplanner.examples.nqueens.domain.Queen;
-import org.optaplanner.examples.nqueens.persistence.NQueensGenerator;
-*/
 
 
 
@@ -67,12 +56,9 @@ import org.optaplanner.examples.nqueens.persistence.NQueensGenerator;
        //vakjesList.add(vak2);
        sudoku.vakjesList=vakjesList;
        	   
-        
-        //// Load a problem with 8 queens
-        //NQueens unsolved8Queens = new NQueensGenerator(true).createNQueens(8);
+     
 
         // Solve the problem
-        //NQueens solved8Queens = solver.solve(unsolved8Queens);
        Sudoku solvedSudoku = solver.solve(sudoku);
        for(int i = 0; i < n; i++) {
     	   for(int j = 0; j < n; j++) {
@@ -93,30 +79,7 @@ import org.optaplanner.examples.nqueens.persistence.NQueensGenerator;
        //System.out.println(solvedSudoku.vakjesList.get(0).cijfer);
        //System.out.println(solvedSudoku.vakjesList.get(1).cijfer);
        
-        // Display the result
-       // System.out.println("\nSolved 8 queens:\n" + toDisplayString(solved8Queens));
+
     }
-/*
-    public static String toDisplayString(NQueens nQueens) {
-        StringBuilder displayString = new StringBuilder();
-        int n = nQueens.getN();
-        List<Queen> queenList = nQueens.getQueenList();
-        for (int row = 0; row < n; row++) {
-            for (int column = 0; column < n; column++) {
-                Queen queen = queenList.get(column);
-                if (queen.getColumn().getIndex() != column) {
-                    throw new IllegalStateException("The queenList is not in the expected order.");
-                }
-                displayString.append(" ");
-                if (queen.getRow() != null && queen.getRow().getIndex() == row) {
-                    displayString.append("Q");
-                } else {
-                    displayString.append("_");
-                }
-            }
-            displayString.append("\n");
-        }
-        return displayString.toString();
-    }
-*/
+
 }
